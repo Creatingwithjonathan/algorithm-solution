d88b05e151e49a6369d04c187e462753f8be4693
Jonathan Darling <40698790+Creatingwithjonathan@users.noreply.github.com>
2018-08-15 11:03:06 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* question 1 http://www.techiedelight.com/find-pair-with-given-sum-array/ 
-	Done by:Jonathan Darling
-	Date:8/15 */
+/*Problem 2 http://www.techiedelight.com/check-subarray-with-0-sum-exists-not/ */
+struct stack{
+int current_size;
+int* current_arr;
+}li;
 int main()
-{   int siz_input=0;
+{
+    int siz =0;
     int i =0;
-    int t = 0;
-    int target_value= 0;
-    printf("Please enter the size of the input");
-    scanf("%d",&siz_input);
-    int *arr = (int*)malloc(sizeof(int)*siz_input);
-    while(i<siz_input){
-        printf("Please enter the %d value",i);
+
+    int target = 0;
+    printf("Please enter the size of the array you would like: ");
+    scanf("%d",&siz);
+    int* arr = (int*)malloc(sizeof(int)*siz);
+    while(i<siz){
+        printf("Please enter the %d digit: ",(i+1));
         scanf("%d",&arr[i]);
         i++;
     }
-    printf("Please enter the target value to find the sum for \n");
-    scanf("%d",&target_value);
-    int num_of_success = 0;
-    for(i=0;i<siz_input;i++){
-        for(t=i;t<siz_input;t++){
-        int a = arr[i];
-        int b = arr[t];
-        int c = a+b;
+     int outer = 0;
+     int inner = 0;
+     int num_of_cycles=1;
+  
+  for (inner=0;inner<siz;inner++){
+      outer =inner;
+        int sum1 = 0;
+		struct stack*stack=(struct stack*)malloc(sizeof(struct stack));
+		stack->current_size = -1;
+		stack->current_arr=(int*)malloc(sizeof(int)*siz);
+    for (outer=inner;outer<siz;outer++){
+            int b = arr[outer];
 
-            if(c == target_value&& a!=b){
-                printf("The value of %d and %d sum up to %d\n",a,b,target_value);
-
-            }
+            sum1= sum1+b;
+            stack->current_size=1+(stack->current_size);
+            stack->current_arr[stack->current_size]=b;
+     
+            if(sum1 != target &&(outer==siz-1)&&(inner==siz-1)){
+                free(stack);
+                outer= siz;
+                inner=siz;
+                }
+            if(sum1 == target){
+                    printf("\t\t\t\t new cycle [%d]\n",num_of_cycles);
+                    printf("\b\tThe total is:%d the target is %d\n",sum1,target);
+                while(stack->current_size!=-1){
+                   printf("\tthe results are: %d\n",stack->current_arr[stack->current_size]);
+                stack->current_size = (stack->current_size)-1;
+                    }
+                        num_of_cycles++;
+                    }
+            if(outer == (siz -1)){
+               free(stack);
+                outer= siz;
+                printf("\n");
+                }
         }
     }
-    if (num_of_success == 0){printf("Sorry there were no two numbers that sum up to the target of %d",target_value);}
     return 0;
 }
